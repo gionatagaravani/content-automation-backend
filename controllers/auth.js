3809f358a3dea953419403debe84b851ad27f226
Gionata Garavani <gionatagaravani@libero.it>
2024-05-14 23:50:37 +0200

    registerLogin

--- controllers/auth.js
+++ controllers/auth.js
@@ -40,6 +40,53 @@ export const register = async (req, res) => {
   }
 };
 
+export const registerLog = async (req, res) => {
+  const { name, surname, email, password } = req.body;
+
+  if (!email) {
+    return res
+      .status(400)
+      .json({ status: "error", message: "email not vaild" });
+  }
+
+  if (!password) {
+    return res
+      .status(400)
+      .json({ status: "error", message: "password not vaild" });
+  }
+
+  if (password.length < 7) {
+    return res.status(400).json({
+      status: "error",
+      message: "password must be at least 8 characters",
+    });
+  }
+
+  const hashPsw = await bcrypt.hash(password, 10);
+  const user = new User({
+    name: name,
+    surname: surname,
+    email: email,
+    password: hashPsw,
+  });
+
+  try {
+    await user.save();
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+        username: email,
+      },
+      process.env.JWT_SECRECT
+    );
+
+    res.status(201).json({ status: "ok", token: token, data: user });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
 export const login = async (req, res) => {
   const { email, password } = req.body;
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { login, register } from '../controllers/auth.js'
+import { login, register, registerLog } from '../controllers/auth.js'
 
 const router = express.Router();
 
 router.post('/register', register);
+router.post('/registerlogin', registerLog);
 router.post('/login', login);
 
 export default router;
